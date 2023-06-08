defd1d8d98d32610f73194e8f65bbd5d16b3be03
Stela Palii <stelasimonapalii@gmail.com>
2023-06-07 18:03:28 -0700

    Update solution to LeetCode problem # 1150

--- leetcode/check-straight-line-solution.js
+++ leetcode/check-straight-line-solution.js
@@ -3,16 +3,18 @@
  * https://leetcode.com/problems/check-if-it-is-a-straight-line
  */
 
-function isStraight([[ax, ay], [bx, by], [cx, cy]]) {
+function haveTheSameSlope([ax, ay], [bx, by], [cx, cy]) {
+  /*
+    Need to check: (by - ay) / (bx - ax) === (cy - by) / (cx - bx)
+    Crossmultiplying to avoid using division 
+  */
+
   return (by - ay) * (cx - bx) === (cy - by) * (bx - ax);
 }
 
 function checkStraightLine(coordinates) {
-  if (coordinates.length === 2) {
-    return true;
-  }
   for (let i = 2; i < coordinates.length; ++i) {
-    if (!isStraight([coordinates[0], coordinates[1], coordinates[i]])) {
+    if (!haveTheSameSlope(coordinates[0], coordinates[1], coordinates[i])) {
       return false;
     }
   }
